ab76abf12691fbd059c5f63ef1b66faa8144695a
vamsi230 <veerendra.dharmavarapu@gmail.com>
2026-09-14 16:35:30 +0530

    Updating UI

--- Source/Stadium_Blockout/GAS/VS_AttributeSet.h
+++ Source/Stadium_Blockout/GAS/VS_AttributeSet.h
@@ -18,19 +18,19 @@ class STADIUM_BLOCKOUT_API UVS_AttributeSet : public UAttributeSet
 	
 public:
 	
-	UPROPERTY(EditDefaultsOnly, Category="Attributes")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Attributes")
 	FGameplayAttributeData CurrentHealth = 100.f;
 	ATTRIBUTE_ACCESSORS_BASIC(UVS_AttributeSet, CurrentHealth)
 	
-	UPROPERTY(EditDefaultsOnly, Category="Attributes")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Attributes")
 	FGameplayAttributeData MaxHealth = 100.f;
 	ATTRIBUTE_ACCESSORS_BASIC(UVS_AttributeSet, MaxHealth)
 	
-	UPROPERTY(EditDefaultsOnly, Category="Attributes")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Attributes")
 	FGameplayAttributeData CurrentStamina = 100.f;
 	ATTRIBUTE_ACCESSORS_BASIC(UVS_AttributeSet, CurrentStamina)
 	
-	UPROPERTY(EditDefaultsOnly, Category="Attributes")
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category="Attributes")
 	FGameplayAttributeData MaxStamina = 100.f;
 	ATTRIBUTE_ACCESSORS_BASIC(UVS_AttributeSet, MaxStamina)
 	
